ca56b58e0b9cda80b8c1c2378dda26fbac946d60
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-18 03:24:58 +0500

    Added contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title', 'Contact')
+
+@section('content')
+    <div class="content">
+        Contact Page
+    </div>
+    @include('partials.testimonials')
+    @include('partials.subscribe')
+@endsection
