fcc69cabb8a99788a847512aaf3ed8f0ccfbd29b
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-18 03:24:47 +0500

    Changes to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.master')
 
-@section('title', 'Home Page')
+@section('title', 'Home')
 
 @section('content')
     <div class="content"></div>
+    @include('partials.slider')
     @include('partials.testimonials')
     @include('partials.subscribe')
 @endsection
