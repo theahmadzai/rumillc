d5aa0e53c4f7ee1a8b887ca7d48c124a3137938d
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-22 04:08:16 +0500

    Image table migration

---
+++ database/migrations/2018_05_21_183919_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('url');
+            $table->string('format');
+            $table->integer('size');
+            $table->enum('type', ['s','g','o'])->default('o');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
