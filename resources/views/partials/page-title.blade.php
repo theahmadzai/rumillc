fefa895dec92cb97bb71f4dfb229269dd5b6265e
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-22 04:10:11 +0500

    page title partial

---
+++ resources/views/partials/page-title.blade.php
@@ -0,0 +1,4 @@
+<div style="position:relative;">
+    <img style="height:400px; width:100%;" src="{{ asset($image) }}" alt="">
+    <h1 style="position:absolute;top:50%; left:50%;color:#fff; transform:translate(-50%,-50%); font-size:5rem; font-weight:300;">{{ $title }}</h1>
+</div>
