382daa9dc7a13922446ecd11e1ad5597b7b8972d
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-03 02:37:44 +0500

    added language handler

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,5 +4,9 @@
             <img src="{{ asset('img/logo.png') }}" alt="Rumi Trading LLC Logo">
         </a>
         @include('partials.navbar')
+        <div style="background:#efefef; padding:1rem; border-radius:5px;">
+            <a href="/" style="text-decoration:none;color:black;">EN</a>/
+            <a href="/pa" style="text-decoration:none;color:black;">PA</a>
+        </div>
     </div>
 </header>
