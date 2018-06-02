60230c3e57f57475dfe4ef197b762e21cec641c4
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-03 02:36:24 +0500

    Added english routes

---
+++ resources/lang/en/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'contact' => 'Contact Us',
+    'about' => 'About Us',
+    'gallery' => 'Photo Gallery',
+    'services' => 'Products & Services',
+    'dubai' => 'Rumi Dubai Office',
+    'indonesia' => 'Rumi Indonesia Office'
+];
