f4f34718807d7fe00831a7fb33113173c293a1e8
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-12 01:23:49 +0500

    modified

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,9 +3,18 @@
 @section('title', 'Contact')
 
 @section('content')
-    @include('partials.page-title', ['title'=>'Contact', 'image'=>'img/slider/s15.jpeg'])
     <div class="content">
-        Contact Page
+    <gmap-component></gmap-component>
+        <div class="contact">
+            <div class="contact__info">
+                <h1>Contact Info</h1>
+                <p><b>Main office: </b>Kolola Pushta road, Burj Barq Street, Kabul district 4 - Afghanistan</p>
+                <p><b>Factory: </b>Qalai Amir, Paghman district, Kabul - Afghanistan</p>
+                <p><b>Contact: </b> +93 (0) 784516129</p>
+                <p><b>Email: </b>info@rumillc.com</p>
+            </div>
+            <contact-component></contact-component>
+        </div>
     </div>
     @include('partials.testimonials')
     @include('partials.subscribe')
