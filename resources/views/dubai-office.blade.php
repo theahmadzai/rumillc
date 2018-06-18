606d030fd68e8a49be7c512318e863dc7f831d37
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-18 13:25:37 +0500

    created

---
+++ resources/views/dubai-office.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', 'About')
+
+@section('content')
+    <div class="content">
+        Dubai Office
+    </div>
+    @include('partials.testimonials')
+@endsection
