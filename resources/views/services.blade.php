265e843c9589fc1bf1a1e7af7f068e0e743c4ed9
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-18 13:25:41 +0500

    created

---
+++ resources/views/services.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', 'About')
+
+@section('content')
+    <div class="content">
+        Services
+    </div>
+    @include('partials.testimonials')
+@endsection
