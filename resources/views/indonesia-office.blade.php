192484dacee7910eee2f44711168c47a2078a8b7
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-18 13:25:29 +0500

    created

---
+++ resources/views/indonesia-office.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', 'About')
+
+@section('content')
+    <div class="content">
+        Indonesia Office
+    </div>
+    @include('partials.testimonials')
+@endsection
