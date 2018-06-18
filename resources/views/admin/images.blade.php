c4335d8fcc74da5067a0749f395ce93aa11baee1
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-18 13:25:45 +0500

    created

--- resources/views/admin/images.blade.php
+++ resources/views/admin/images.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.admin')
 
 @section('panel')
-    Images
+    <div>
+        @foreach ($slider as $image)
+            <img src="/storage/app/{{ $image->url }}" alt="{{ $image->title }}">
+        @endforeach
+    </div>
+
+    <form action="images" method="post" enctype="multipart/form-data" style="display:flex;flex-direction:column;width:300px;padding:2rem;">
+
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" value="{{ old('title') }}">
+
+        <input type="radio" name="type" value="s" id="slider">
+        <label for="slider">Slider</label>
+
+        <input type="radio" name="type" value="g" id="gallery">
+        <label for="gallery">Gallery</label>
+
+        <input type="radio" name="type" value="o" id="other" checked>
+        <label for="other" selected>Other</label>
+
+        <label for="image">Image</label>
+        <input type="file" name="image" id="image">
+
+        @csrf
+        <input type="submit" value="Upload">
+    </form>
 @endsection
