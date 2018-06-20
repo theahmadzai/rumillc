eb9fa23b124717b0ef87ba249b228508e31b43cc
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-21 00:47:22 +0500

    added page controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function services()
+    {
+        return view('services');
+    }
+
+    public function dubai()
+    {
+        return view('dubai-office');
+    }
+
+    public function indonesia()
+    {
+        return view('indonesia-office');
+    }
+
+    public function gallery()
+    {
+        return view('gallery');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
