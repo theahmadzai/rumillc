fffd3d5e596956e9bdc8910da1ad8a7008044cd6
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-22 00:25:27 +0500

    updated admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,21 +3,39 @@
 @section('title', 'Admin')
 
 @section('content')
-    <div class="admin-heading">
-        <h1>Admin Panel</h1>
-        <h2>Welcome {{Auth::user()->name}}</h2>
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button>Logout</button>
-        </form>
-    </div>
-    <div class="admin-panel">
-        <div class="left-pane">
-            <a href="{{ URL::route('admin.images') }}">Add Image</a>
-            <a href="{{ URL::route('admin.testimonials') }}">Add Testimonial</a>
-        </div>
-        <div class="right-pane">
-            @yield('panel')
+    <div class="container">
+        <section class="hero is-primary is-link">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">Admin Dashboard</h1>
+                    <h2 class="subtitle">Welcome, {{ Auth::user()->name }}</h2>
+                </div>
+            </div>
+        </section>
+
+        <div class="columns is-marginless has-background-white">
+            <div class="column is-one-fifth">
+                <aside class="menu">
+                    <p class="menu-label">Administration</p>
+                    <ul class="menu-list">
+                        <li><a href="/admin" class="{{ request()->is('admin') ? 'is-active' : '' }}">Home</a></li>
+                        <li><a href="/admin/images" class="{{ request()->is('admin/images') ? 'is-active' : '' }}">Images</a></li>
+                        <li><a href="/admin/testimonials" class="{{ request()->is('admin/testimonials') ? 'is-active' : '' }}">Testimonials</a></li>
+                    </ul>
+                    <p class="menu-label">Account</p>
+                    <ul class="menu-list">
+                        <li><a href="">Account Info</a></li>
+                        <li><a href="">Settings</a></li>
+                        <li><a href="{{ route('logout') }}">Logout</a></li>
+                    </ul>
+                </aside>
+            </div>
+
+            <div class="column">
+                <div class="section">
+                    @yield('panel')
+                </div>
+            </div>
         </div>
     </div>
 @endsection
