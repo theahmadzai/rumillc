b12a53e8d8454a211367fc3707abcec0f9251a3c
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-21 16:17:29 +0500

    changed to image api resource

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('images', 'ImageController');
+Route::apiResource('images', 'ImageController');
