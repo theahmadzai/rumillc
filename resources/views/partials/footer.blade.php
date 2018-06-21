aeb4b76fece431dafce1f5ebc15631644fac02b7
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-21 16:20:31 +0500

    modified

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -17,8 +17,8 @@
                 <a href="{{ URL::route('home') }}">@lang('routes.home')</a>
                 <a href="{{ URL::route('about') }}">@lang('routes.about')</a>
                 <a href="{{ URL::route('services') }}">@lang('routes.services')</a>
-                <a href="{{ URL::route('dubai') }}">@lang('routes.dubai')</a>
-                <a href="{{ URL::route('indonesia') }}">@lang('routes.indonesia')</a>
+                <a href="{{ URL::route('dubai-office') }}">@lang('routes.dubai')</a>
+                <a href="{{ URL::route('indonesia-office') }}">@lang('routes.indonesia')</a>
                 <a href="{{ URL::route('gallery') }}">@lang('routes.gallery')</a>
                 <a href="{{ URL::route('contact') }}">@lang('routes.contact')</a>
             </div>
