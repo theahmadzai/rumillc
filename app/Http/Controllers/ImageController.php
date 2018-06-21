770f543afb54cf6bf9a6cc82bda1cf36307eee4f
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-21 16:20:25 +0500

    image controller modified

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Image;
+use Storage;
 use App\Http\Resources\ImageResource;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,15 @@ class ImageController extends Controller
      */
     public function index()
     {
-        return ImageResource::collection(Image::all());
+        if(!\Request::has('type')) {
+            return ImageResource::collection(Image::all());
+        }
+
+        $type = \Request::query('type');
+
+        if(in_array($type, ['o','s','g'])){
+            return ImageResource::collection(Image::where('type', $type)->get());
+        }
     }
 
     /**
@@ -36,7 +45,54 @@ class ImageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $title = $request->title;
+
+        if(!$request->filled('title')
+        || strlen($title) < 5
+        || strlen($title) > 255){
+            return redirect('admin/images')->withInput(
+                $request->except('image')
+            );
+        }
+
+        $image = $request->image;
+
+        if(!$request->hasFile('image')
+        || !$request->file('image')->isValid()
+        || !in_array($image->extension(), ['jpg','jpeg','png'])) {
+            return redirect('admin/images')->withInput(
+                $request->expect('image')
+            );
+        }
+
+        $type = $request->type;
+
+
+        $dirs = [
+            's' => 'slider',
+            'g' => 'gallery',
+            'o' => 'others'
+        ];
+
+        if(!in_array($type,array_keys($dirs))) {
+            return redirect('admin/images')->withInput(
+                $request->expect('image')
+            );
+        }
+
+        $path = $image->store($dirs[$type]);
+
+        $i = new Image;
+
+        $i->title = $title;
+        $i->format = $image->extension();
+        $i->size = $image->getSize();
+        $i->url = $path;
+        $i->type = $type;
+
+        $i->save();
+
+        return redirect()->route('admin.images');
     }
 
     /**
@@ -47,7 +103,7 @@ class ImageController extends Controller
      */
     public function show(Image $image)
     {
-        //
+        return new ImageResource($image);
     }
 
     /**
@@ -81,6 +137,10 @@ class ImageController extends Controller
      */
     public function destroy(Image $image)
     {
-        //
+        Storage::delete($image->url);
+
+        Image::destroy($image->id);
+
+        return redirect()->route('admin.images');
     }
 }
