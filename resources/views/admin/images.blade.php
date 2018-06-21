fbd3cd94f7c5fc99cf996825917fc44136dee6f6
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-22 00:25:59 +0500

    updated

--- resources/views/admin/images.blade.php
+++ resources/views/admin/images.blade.php
@@ -2,38 +2,48 @@
 
 @section('panel')
 
-    @if (Session::has('error'))
-        <div>{{ Session::get('error') }}</div>
+    @if (session('status'))
+        <div class="notification is-success">
+            <button class="delete" onclick="this.parentElement.style.display='none';"></button>
+            {{ session('status') }}
+        </div>
     @endif
 
-    <form action="/api/images" method="post" enctype="multipart/form-data">
-        @csrf
+    @include('admin.partials.image-upload-form')
 
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title" value="{{ old('title') }}">
+    <div class="section">
+        <div class="tabs">
+            <ul>
+                <li class="{{ app('request')->input('type') ? '' : 'is-active' }}"><a href="images">All</a></li>
+                <li class="{{ app('request')->input('type') == 's' ? 'is-active' : '' }}"><a href="images?type=s">Slider</a></li>
+                <li class="{{ app('request')->input('type') == 'g' ? 'is-active' : '' }}"><a href="images?type=g">Gallery</a></li>
+                <li class="{{ app('request')->input('type') == 'o' ? 'is-active' : '' }}"><a href="images?type=o">Other</a></li>
+            </ul>
+        </div>
 
-        <label for="type">Category</label>
-        <select name="type" id="type">
-            <option value="s">Slider</option>
-            <option value="g">Gallery</option>
-            <option value="o" selected>Other</option>
-        </select>
-
-        <label for="image">Image</label>
-        <input type="file" name="image" id="image">
-
-        <input type="submit" value="Upload">
-    </form>
-
-    <div>
-        @foreach ($images as $image)
-            <div>
-                <img src="/storage/app/{{ $image->url }}" alt="{{ $image->title }}">
-                <form action="/api/images/{{ $image->id }}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" value="delete">
-                </form>
+        @foreach ($images->chunk(3) as $chunk)
+            <div class="columns">
+                @foreach ($chunk as $image)
+                    <div class="column is-one-third">
+                        <div class="card">
+                            <div class="card-header">
+                                <p class="card-header-title">
+                                    {{ str_limit($image->title, 20, "...") }}
+                                </p>
+                                <form method="POST" action="/api/images/{{ $image->id }}" class="card-header-icon">
+                                    @csrf
+                                    @method('delete')
+                                    <a href="#" class="icon delete" onclick="this.parentElement.submit();">Delete</a>
+                                </form>
+                            </div>
+                            <div class="card-image">
+                                <figure class="image">
+                                    <img src="/storage/app/{{ $image->url }}" alt="{{ $image->title }}">
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         @endforeach
     </div>
