aa33b1b2fe0b0798e9e7899d302b4bb28ad84e17
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-22 00:25:16 +0500

    added bulma css framework

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,7 @@
         <title>{{ config('app.name') }} - @yield('title')</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
