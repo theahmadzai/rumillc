2a93135cba2bd3e721e5cd3922681096cb9d66fc
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-22 11:37:27 +0500

    added user table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ImagesTableSeeder::class
+            ImagesTableSeeder::class,
+            UsersTableSeeder::class
         ]);
     }
 }
