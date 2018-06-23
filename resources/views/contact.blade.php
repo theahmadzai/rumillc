606646e751c01a687bdf7e91ed79bcb5ed1d5904
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-24 01:28:20 +0500

    updated

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,19 +3,23 @@
 @section('title', 'Contact')
 
 @section('content')
-    <div class="content">
+
     <gmap-component></gmap-component>
-        <div class="contact">
-            <div class="contact__info">
-                <h1>Contact Info</h1>
-                <p><b>Main office: </b>Kolola Pushta road, Burj Barq Street, Kabul district 4 - Afghanistan</p>
-                <p><b>Factory: </b>Qalai Amir, Paghman district, Kabul - Afghanistan</p>
-                <p><b>Contact: </b> +93 (0) 784516129</p>
-                <p><b>Email: </b>info@rumillc.com</p>
-            </div>
-            <contact-component></contact-component>
+
+    <div class="contact">
+        <div class="contact__info">
+            <h1>Contact Info</h1>
+            <p><b>Main office: </b>Kolola Pushta road, Burj Barq Street, Kabul district 4 - Afghanistan</p>
+            <p><b>Factory: </b>Qalai Amir, Paghman district, Kabul - Afghanistan</p>
+            <p><b>Contact: </b> +93 (0) 784516129</p>
+            <p><b>Email: </b>info@rumillc.com</p>
         </div>
+
+        <contact-component></contact-component>
     </div>
-    @include('partials.testimonials')
-    @include('partials.subscribe')
+
+    <testimonials-component></testimonials-component>
+
+    <subscribe-component></subscribe-component>
+
 @endsection
