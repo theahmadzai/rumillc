d1b2f96a5642fc01a0bde820c7fa7a8e32cf8253
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-24 01:27:58 +0500

    deleted and use component directly

--- resources/views/partials/subscribe.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="subscribe">
-    <form action="">
-        <input type="email" placeholder="example@mail.com" class="subscribe__input">
-        <input type="submit" value="subscribe" class="subscribe__submit">
-    </form>
-</div>
