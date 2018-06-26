c6baf4470b80e38208c1ca5ba261388f93c98557
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-27 01:39:25 +0500

    added lang file

---
+++ resources/lang/en/contact.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'address' => [
+        'h' => 'Main office',
+        'm' => 'Kolola Pushta road, Burj Barq Street, Kabul district 4 - Afghanistan',
+    ],
+    'factory' => [
+        'h' => 'Factory',
+        'm' => 'Qalai Amir, Paghman district, Kabul - Afghanistan'
+    ],
+    'phone' => [
+        'h' => 'Contact',
+        'm' =>  '+93 (0) 784516129'
+    ],
+    'email' => [
+        'h' => 'Email',
+        'm' => 'info@rumillc.com'
+    ]
+];
