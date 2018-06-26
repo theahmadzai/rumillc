5f38f7d77de4a99cb3d95c57cff05e6706523896
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-27 01:39:47 +0500

    english langs for misc

---
+++ resources/lang/en/misc.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'follow_us' => 'Subscribe to our RSS feed or follow us on Facebook and Twitter for the latest content',
+    'copyrights' => '&copy; Copyright 2017-2018 - Rumi Trading LLC'
+];
