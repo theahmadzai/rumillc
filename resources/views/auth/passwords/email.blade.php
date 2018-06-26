22cefd0e61c99e506fdb03a11cb3b3034c9710a9
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-26 23:04:29 +0500

    modified

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,31 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="container">
+        <div class="section" style="max-width:400px; margin:0 auto;">
 
-    @if (session('status'))
-        <div>
-            {{ session('status') }}
-        </div>
-    @endif
-
-    <form method="POST" action="{{ route('password.email') }}" style="width:30%; padding:2rem;" novalidate>
-        @csrf
-
-        <div class="field">
-            <label for="email" class="label">Email</label>
-            <div class="control">
-                <input id="email" type="email" name="email" value="{{ $email ?? old('email') }}" class="input" required autofocus>
-            </div>
-            @if ($errors->has('email'))
-                <p class="help is-danger">{{ $errors->first('email') }}</p>
+            @if (session('status'))
+                <div class="notification is-info">
+                    <button class="delete" onclick="this.parentElement.style.display='none';"></button>
+                    {{ session('status') }}
+                </div>
             @endif
-        </div>
 
-        <div class="field">
-            <div class="control">
-                <button class="button is-link">Send Password Reset Link</button>
-            </div>
+            <form method="POST" action="{{ route('password.email') }}" novalidate>
+                @csrf
+
+                <div class="field">
+                    <label for="email" class="label">Email</label>
+                    <div class="control">
+                        <input id="email" type="email" name="email" value="{{ $email ?? old('email') }}" class="input" required autofocus>
+                    </div>
+                    @if ($errors->has('email'))
+                        <p class="help is-danger">{{ $errors->first('email') }}</p>
+                    @endif
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-link">Send Password Reset Link</button>
+                    </div>
+                </div>
+            </form>
+
         </div>
-    </form>
+    </div>
 </div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="container">
+        <div class="section" style="max-width:400px; margin:0 auto;">
 
-    @if (session('status'))
-        <div>
-            {{ session('status') }}
-        </div>
-    @endif
-
-    <form method="POST" action="{{ route('password.request') }}" style="width:30%; padding:2rem;" novalidate>
-        @csrf
-
-        <input type="hidden" name="token" value="{{ $token }}">
-
-        <div class="field">
-            <label for="email" class="label">Email</label>
-            <div class="control">
-                <input id="email" type="email" name="email" value="{{ $email ?? old('email') }}" class="input" required autofocus>
-            </div>
-            @if ($errors->has('email'))
-                <p class="help is-danger">{{ $errors->first('email') }}</p>
+            @if (session('status'))
+                <div class="notification is-info">
+                    <button class="delete" onclick="this.parentElement.style.display='none';"></button>
+                    {{ session('status') }}
+                </div>
             @endif
-        </div>
 
-        <div class="field">
-            <label for="password" class="label">Password</label>
-            <div class="control">
-                <input id="password" type="password" name="password" class="input" required>
-            </div>
-            @if ($errors->has('password'))
-                <p class="help is-danger">{{ $errors->first('password') }}</p>
-            @endif
-        </div>
+            <form method="POST" action="{{ route('password.request') }}" novalidate>
+                @csrf
 
-        <div class="field">
-            <label for="password-confirm" class="label">Confirm Password</label>
-            <div class="control">
-                <input id="password-confirm" type="password" name="password_confirmation" class="input" required>
-            </div>
-        </div>
+                <input type="hidden" name="token" value="{{ $token }}">
 
-        <div class="field">
-            <div class="control">
-                <button class="button is-link">Reset Password</button>
-            </div>
+                <div class="field">
+                    <label for="email" class="label">Email</label>
+                    <div class="control">
+                        <input id="email" type="email" name="email" value="{{ $email ?? old('email') }}" class="input" required autofocus>
+                    </div>
+                    @if ($errors->has('email'))
+                        <p class="help is-danger">{{ $errors->first('email') }}</p>
+                    @endif
+                </div>
+
+                <div class="field">
+                    <label for="password" class="label">Password</label>
+                    <div class="control">
+                        <input id="password" type="password" name="password" class="input" required>
+                    </div>
+                    @if ($errors->has('password'))
+                        <p class="help is-danger">{{ $errors->first('password') }}</p>
+                    @endif
+                </div>
+
+                <div class="field">
+                    <label for="password-confirm" class="label">Confirm Password</label>
+                    <div class="control">
+                        <input id="password-confirm" type="password" name="password_confirmation" class="input" required>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-link">Reset Password</button>
+                    </div>
+                </div>
+            </form>
+
         </div>
-    </form>
+    </div>
 @endsection
