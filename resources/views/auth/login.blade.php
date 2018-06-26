51c792db988712a1203683281b810f2fa0efe6a8
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-06-26 23:04:11 +0500

    fixed stying

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,52 @@
 @extends('layouts.master')
 
 @section('content')
-    <form method="POST" action="{{ route('login') }}" novalidate style="width:30%; padding:3rem; margin:0 auto;">
-        @csrf
+    <div class="container">
+        <div class="section" style="max-width:400px; margin:0 auto;">
 
-        <div class="field">
-            <label for="email" class="label">Email</label>
-            <div class="control">
-                <input id="email" type="email" name="email" value="{{ old('email') }}" class="input" required autofocus>
-            </div>
-            @if ($errors->has('email'))
-                <p class="help is-danger">{{ $errors->first('email') }}</p>
-            @endif
-        </div>
+            <form method="POST" action="{{ route('login') }}" novalidate>
+                @csrf
 
-        <div class="field">
-            <label for="password" class="label">Password</label>
-            <div class="control">
-                <input id="password" type="password" name="password" class="input" required>
-            </div>
-            @if ($errors->has('password'))
-                <p class="help is-danger">{{ $errors->first('password') }}</p>
-            @endif
-        </div>
+                <div class="field">
+                    <label for="email" class="label">Email</label>
+                    <div class="control">
+                        <input id="email" type="email" name="email" value="{{ old('email') }}" class="input" required autofocus>
+                    </div>
+                    @if ($errors->has('email'))
+                        <p class="help is-danger">{{ $errors->first('email') }}</p>
+                    @endif
+                </div>
 
-        <div class="field">
-            <div class="control">
-                <label class="checkbox">
-                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                    Remember me
-                </label>
-            </div>
-        </div>
+                <div class="field">
+                    <label for="password" class="label">Password</label>
+                    <div class="control">
+                        <input id="password" type="password" name="password" class="input" required>
+                    </div>
+                    @if ($errors->has('password'))
+                        <p class="help is-danger">{{ $errors->first('password') }}</p>
+                    @endif
+                </div>
 
-        <div class="field">
-            <div class="control">
-                <button class="button is-link">Login</button>
-            </div>
-        </div>
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                            Remember me
+                        </label>
+                    </div>
+                </div>
 
-        <div class="field">
-            <a href="{{ route('password.request') }}">Forgotten your password?</a>
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-link">Login</button>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <a href="{{ route('password.request') }}">Forgotten your password?</a>
+                </div>
+            </form>
+
         </div>
-    </form>
+    </div>
 @endsection
