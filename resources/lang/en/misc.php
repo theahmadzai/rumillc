9c19ee80f865a9b1f9795301ba0fed82ce100821
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-03 23:17:55 +0500

    added translations

--- resources/lang/en/misc.php
+++ resources/lang/en/misc.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'about' => 'Rumi Trading LLC (Limited Liability Company) is registered with ACBR (Afghanistan Central Business Registry) of the ministry of commerce & industries of Afghanistan operate as Afghanistan’s leading export management company (EMC) since 2017.',
     'follow_us' => 'Subscribe to our RSS feed or follow us on Facebook and Twitter for the latest content',
-    'copyrights' => '&copy; Copyright 2017-2018 - Rumi Trading LLC'
+    'copyrights' => '&copy; Copyright 2017-2018 - Rumi Trading LLC',
+    'working_time' => 'Monday to Friday (08:00 to 05:00 PM)'
 ];
