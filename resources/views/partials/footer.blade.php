34e7b01b2dd2de65dd7f3b120abd6f36311594b3
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-03 23:17:33 +0500

    modified

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,31 +1,22 @@
 <footer class="footer has-background-dark has-text-light is-paddingless">
     <div class="container">
         <div class="section">
-            <div class="columns">
-                <div class="column is-6">
-                    <div class="content is-small">
-                        <h3 class="title has-text-light is-4">Contact</h3>
-                        <p><strong class="has-text-light">@lang('contact.address.h'):</strong> @lang('contact.address.m')</p>
-                        <p><strong class="has-text-light">@lang('contact.factory.h'):</strong> @lang('contact.factory.m')</p>
-                        <p><strong class="has-text-light">@lang('contact.phone.h'):</strong> @lang('contact.phone.m')</p>
-                        <p><strong class="has-text-light">@lang('contact.email.h'):</strong> @lang('contact.email.m')</p>
+            <div class="columns is-multiline is-8">
+                <div class="column is-5">
+                    <div class="content" style="max-width:350px;">
+                        <h3 class="title has-text-light is-4">About RUMI</h3>
+                        <p class="has-text-justified">@lang('misc.about')</p>
+                    </div>
+                </div>
+                <div class="column is-4">
+                    <div class="content">
+                        <h3 class="title has-text-light is-4">Working Hours</h3>
+                        <p class="has-text-justified">@lang('misc.working_time')</p>
                     </div>
                 </div>
                 <div class="column is-3">
-                    <div class="content b-links is-small">
-                        <h3 class="title has-text-light is-4">Links</h3>
-                        <a href="{{ URL::route('home') }}">@lang('routes.home')</a>
-                        <a href="{{ URL::route('about') }}">@lang('routes.about')</a>
-                        <a href="{{ URL::route('services') }}">@lang('routes.services')</a>
-                        <a href="{{ URL::route('dubai-office') }}">@lang('routes.dubai')</a>
-                        <a href="{{ URL::route('indonesia-office') }}">@lang('routes.indonesia')</a>
-                        <a href="{{ URL::route('gallery') }}">@lang('routes.gallery')</a>
-                        <a href="{{ URL::route('contact') }}">@lang('routes.contact')</a>
-                    </div>
-                </div>
-                <div class="column is-3">
-                    <div class="content is-small">
-                        <h3 class="title has-text-light is-4">Follow Us</h3>
+                    <div class="content">
+                        <h3 class="title has-text-light is-4">Get in Touch</h3>
                         <p>@lang('misc.follow_us')</p>
                         <div class="b-icons">
                             <span class="icon">
@@ -44,6 +35,27 @@
                                 </a>
                             </span>
                         </div>
+                    </div>
+                </div>
+                <div class="column is-5">
+                    <div class="content b-links is-small">
+                        <h3 class="title has-text-light is-4">Sitemap</h3>
+                        <a href="{{ URL::route('home') }}">@lang('routes.home')</a>
+                        <a href="{{ URL::route('about') }}">@lang('routes.about')</a>
+                        <a href="{{ URL::route('services') }}">@lang('routes.services')</a>
+                        <a href="{{ URL::route('dubai-office') }}">@lang('routes.dubai')</a>
+                        <a href="{{ URL::route('indonesia-office') }}">@lang('routes.indonesia')</a>
+                        <a href="{{ URL::route('gallery') }}">@lang('routes.gallery')</a>
+                        <a href="{{ URL::route('contact') }}">@lang('routes.contact')</a>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="content">
+                        <h3 class="title has-text-light is-4">Our Address</h3>
+                        <p><strong>@lang('contact.address.h'):</strong> @lang('contact.address.m')</p>
+                        <p><strong>@lang('contact.factory.h'):</strong> @lang('contact.factory.m')</p>
+                        <p><strong>@lang('contact.phone.h'):</strong> @lang('contact.phone.m')</p>
+                        <p><strong>@lang('contact.email.h'):</strong> @lang('contact.email.m')</p>
                     </div>
                 </div>
             </div>
