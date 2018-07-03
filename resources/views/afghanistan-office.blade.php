bd257ad80a88af6311c33d0d859f65abf964c600
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-03 23:17:38 +0500

    added page

---
+++ resources/views/afghanistan-office.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', 'About')
+
+@section('content')
+    <div class="content">
+        Afghanistan Office
+    </div>
+    <testimonials-component></testimonials-component>
+@endsection
