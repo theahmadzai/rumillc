6a32f153d0b02be04c69e4ca4117aea8cd8f78ee
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-13 15:57:04 +0500

    using partials now

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,19 +7,8 @@
 
         @include('partials.navbar')
 
-        <div class="control has-icons-left">
-            <div class="select is-rounded">
-                <form action="/lang" method="post" onchange="this.submit()">
-                    @csrf
-                    <select name="lang">
-                        <option value="en" {{ LaravelLocalization::getCurrentLocale() == 'en' ? 'selected' : ''}}>EN</option>
-                        <option value="ps" {{ LaravelLocalization::getCurrentLocale() == 'ps' ? 'selected' : ''}}>PS</option>
-                    </select>
-                </form>
-            </div>
-            <div class="icon is-small is-left">
-                <i class="fa fa-globe"></i>
-            </div>
-        </div>
+        @include('partials.langbar')
+
+        @include('partials.toggle')
     </div>
 </header>
