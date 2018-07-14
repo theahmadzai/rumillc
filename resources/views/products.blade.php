c8d05349ab9199d62a86e654b718996ea59ecad2
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-14 21:44:33 +0500

    added products

---
+++ resources/views/products.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title', 'Products')
+
+@section('content')
+    <div class="content">
+        Products
+    </div>
+
+    <testimonials-component></testimonials-component>
+@endsection
