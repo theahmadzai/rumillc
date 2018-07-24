93a048cfb554df48bc8b185ed57fc803316684f4
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-24 17:58:51 +0500

    modified

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -16,6 +16,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug
         ];
