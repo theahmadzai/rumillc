5c1c5d458d91eb46f07da8038bc451849af7cba7
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-24 13:00:41 +0500

    images seeder

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -11,6 +11,6 @@ class ImagesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Image::class, 10)->create();
+        factory(App\Image::class, 15)->create();
     }
 }
