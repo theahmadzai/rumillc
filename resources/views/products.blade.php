39c7e1b64be57374ab5978fc670e4c6b827adc0a
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-24 17:57:03 +0500

    Added categories component

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,9 +3,25 @@
 @section('title', 'Products')
 
 @section('content')
-    <div class="content">
-        Products
+    <div class="container section">
+        <ul class="leftmenu">
+            @foreach ($categories as $category)
+                <li class="leftmenu__item">
+                    <a>{{ $category->name }}</a>
+                    <ul class="submenu">
+                        @foreach ($category->products as $product)
+                            <li class="submenu__item">
+                                <img src="storage/app/{{$product->image}}">
+                                <a href="product/{{$product->id}}-{{$product->slug}}">{{ $product->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            @endforeach
+        </ul>
     </div>
+
+    <categories-component></categories-component>
 
     <testimonials-component></testimonials-component>
 @endsection
