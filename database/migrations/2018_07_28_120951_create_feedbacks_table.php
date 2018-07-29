b6103fedd6c17351c02678c26ba7b50a7f8ee2f0
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-29 19:16:40 +0500

    added

---
+++ database/migrations/2018_07_28_120951_create_feedbacks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rating');
+            $table->string('name');
+            $table->string('title');
+            $table->text('message');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}
