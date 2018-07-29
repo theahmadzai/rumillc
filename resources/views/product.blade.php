d2172b32a5923e49fb5abea8a63635f378f5a0e5
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-29 19:18:35 +0500

    updated

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,19 +4,6 @@
 
 @section('content')
 
-    <div class="section" style="width:60%; margin: 0 auto;">
-        <div style="height:400px; width:100%; overflow:hidden;">
-            <img src="/storage/app/{{$product->image}}" alt="{{$product->name}}">
-        </div>
-        <div class="content">
-            <br>
-            <a href="#">{{$product->category->name}}</a> > <a href="#">{{$product->name}}</a>
-            <br>
-            <h2>{{$product->name}}</h2>
-            <p>{{$product->price}}</p>
-            <hr>
-            <p>{{$product->content}}</p>
-        </div>
-    </div>
+    <product-component id="{{$id}}"></product-component>
 
 @endsection
