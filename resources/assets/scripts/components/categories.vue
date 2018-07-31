cb0db4707f840ca2a3e5eecd1f66a5e21bc14ca3
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-07-31 16:35:22 +0500

    updated

--- resources/assets/scripts/components/categories.vue
+++ resources/assets/scripts/components/categories.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
-      <div class="category" v-for="(category,key) in categories" :key="key">
-        <h2>{{ category.name }}</h2>
-          <products :category="category.id"></products>
+  <div v-if="!loaded" class="loading rel"></div>
+  <div v-else class="container section columns">
+    <nav class="panel column is-one-fifth">
+      <p class="panel-heading">Categories</p>
+      <a class="panel-block" v-for="(category,key) in categories" :key="key" @click="select(key)" :class="{'is-active': selected == category.id}">
+          <span class="panel-icon">
+            <i class="fas fa-shopping-basket"></i>
+          </span>
+          {{category.name}}
+      </a>
+      <div class="panel-block">
+          <button class="button is-link is-outlined is-fullwidth" @click="reload">
+            Refresh
+          </button>
       </div>
+    </nav>
+    <div class="column">
+      <products :category="selected" v-model="selected"></products>
+    </div>
   </div>
 </template>
 
@@ -13,17 +27,28 @@ import Products from './products';
 export default {
   data() {
     return {
-      categories: {}
+      loaded    : false,
+      categories: {},
+      selected  : null
     };
   },
   mounted() {
     this.getCategories();
   },
   methods: {
+    select(key) {
+      this.selected = this.categories[key].id;
+    },
+    reload() {
+      this.selected = null;
+      this.getCategories();
+    },
     async getCategories() {
       try {
+        this.loaded = false;
         const response = await axios.get('/api/categories');
         this.categories = response.data;
+        this.loaded = true;
       } catch (error) {
         console.log(error);
       }
