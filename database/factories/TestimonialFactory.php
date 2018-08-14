8ee6022fdd3a0e366d96e986c2fc0bd4316c2ae5
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-15 03:35:03 +0500

    remove image generation

--- database/factories/TestimonialFactory.php
+++ database/factories/TestimonialFactory.php
@@ -5,7 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Testimonial::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'image' => "../temp/testimonials/{$faker->numberBetween(1,3)}.jpeg",
         'message' => $faker->sentence(20)
     ];
 });
