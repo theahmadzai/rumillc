312a9f00fad850375d6349cb0903ba4bcd5b68c0
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-15 03:34:43 +0500

    image nullable

--- database/migrations/2018_06_22_180445_create_testimonials_table.php
+++ database/migrations/2018_06_22_180445_create_testimonials_table.php
@@ -16,7 +16,7 @@ class CreateTestimonialsTable extends Migration
         Schema::create('testimonials', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->text('message');
             $table->timestamps();
         });
