a99ca3525b1cb59a0f9d62b2f6092c69b8dc9729
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-15 03:34:19 +0500

    added storage path

--- app/Testimonial.php
+++ app/Testimonial.php
@@ -6,4 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Testimonial extends Model
 {
+    public function getImageAttribute($image)
+    {
+        return '/storage/app/' . ($image ?? 'public/default_images/testimonial.png');
+    }
 }
