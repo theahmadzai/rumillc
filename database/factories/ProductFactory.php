eecfd3edde075ca11726ea5cb42bfbd22bc9d70f
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-15 03:35:12 +0500

    remove image generation

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,6 @@ $factory->define(App\Product::class, function (Faker $faker) use ($factory) {
     return [
         'name' => $faker->word,
         'slug' => $faker->slug,
-        'image' => "../temp/images/{$faker->numberBetween(1, 20)}.jpeg",
         'price' => $faker->numberBetween(1000, 20000),
         'tags' => $faker->word,
         'content' => $faker->text(500),
