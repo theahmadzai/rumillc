f11850a8181ba6d152f8db9c50c8a50e051d91d5
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-14 17:42:56 +0500

    added tags

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,12 +2,13 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Product::class, function (Faker $faker) use ($factory){
+$factory->define(App\Product::class, function (Faker $faker) use ($factory) {
     return [
         'name' => $faker->word,
         'slug' => $faker->slug,
-        'image' => "../temp/images/{$faker->numberBetween(1,20)}.jpeg",
+        'image' => "../temp/images/{$faker->numberBetween(1, 20)}.jpeg",
         'price' => $faker->numberBetween(1000, 20000),
+        'tags' => $faker->word,
         'content' => $faker->text(500),
         'category_id' => $factory->create(App\Category::class)->id,
     ];
