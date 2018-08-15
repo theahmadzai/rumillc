6b13ec443e594cd83bf51dbc54b0662811e9e22c
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-15 15:34:29 +0500

    remove default image

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -5,9 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Image::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(6),
-        'url' => "../temp/images/{$faker->numberBetween(1,20)}.jpeg",
         'format' => $faker->fileExtension(),
-        'type' => $faker->randomElement(['s','g','o']),
+        'type' => $faker->randomElement(['s', 'g', 'o']),
         'size' => $faker->numberBetween(1000, 8000),
     ];
 });
