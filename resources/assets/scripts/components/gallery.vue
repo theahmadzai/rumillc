04f0500ed71e824eb8634379e9fdb5a3040942cc
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-17 20:50:12 +0500

    updated

--- resources/assets/scripts/components/gallery.vue
+++ resources/assets/scripts/components/gallery.vue
@@ -1,44 +1,36 @@
 <template>
   <div class="section">
+    <input type="range" class="range-gallery" step="3" min="3" max="9" v-model.number=offset @change=loadImages>
 
-    <popper trigger="hover" :options="{placement: 'top'}">
-      <div class="popper">
-          Slide left for slow networks
+    <div v-if=loading class="loading rel"></div>
+    <div v-else>
+      <pagination :pagination=pagination :offset=offset @paginate=loadImages></pagination>
+
+      <gallery :images=images :options=options :index=index @close="index = null"></gallery>
+
+      <div class="columns is-multiline is-8">
+        <image-component v-for="(image, key) in images" :key=key :image=image @click.native="index = key"></image-component>
       </div>
-
-      <input slot="reference" class="range" step="3" min="3" max="9" v-model.number="offset" type="range" @change="loadImages()">
-    </popper>
-
-    <pagination class="pagination" :pagination="pagination" :offset="offset" @paginate="loadImages()"></pagination>
-
-    <div v-if="this.loading==true" class="loading"></div>
-
-    <div class="images">
-      <gallery :images="images" :options="options" :index="index" @close="index = null"></gallery>
-      <figure v-for="(image, key) in images" :key="key">
-        <img :src="image.url" @click="index = key">
-      </figure>
     </div>
-
   </div>
 </template>
 
 <script>
 import VueGallery from 'vue-gallery';
 import Pagination from './pagination';
-import Popper from 'vue-popperjs';
+import Image from './image';
 
 export default {
   data() {
     return {
+      loading: true,
       index  : null,
       options: {
         urlProperty: 'url'
       },
       images    : [],
       pagination: {},
-      offset    : 9,
-      loading   : true
+      offset    : 9
     };
   },
   mounted() {
@@ -47,7 +39,6 @@ export default {
   methods: {
     async loadImages() {
       this.loading = true;
-
       try {
         const response = await axios.get('/api/images', {
           params: {
@@ -63,93 +54,12 @@ export default {
       } catch (error) {
         console.log(error);
       }
-    },
-    changeOffset(val) {
-      this.offset = val;
-      console.log(val);
     }
   },
   components: {
-    gallery   : VueGallery,
-    pagination: Pagination,
-    popper    : Popper
+    gallery          : VueGallery,
+    pagination       : Pagination,
+    'image-component': Image
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.popper {
-  position: absolute;
-  z-index: 200000;
-  display: inline-block;
-  width: auto;
-  margin-bottom: 10px;
-  padding: 4px 8px;
-  border: 1px #111111 solid;
-  border-radius: 3px;
-  background-color: #000000;
-  -moz-box-shadow: rgb(0, 0, 0) 0 0 6px 0;
-  -webkit-box-shadow: rgb(0, 0, 0) 0 0 6px 0;
-  box-shadow: rgb(0, 0, 0) 0 0 6px 0;
-  font-size: 14px;
-  font-weight: normal;
-  color: #ffffff;
-  text-align: center;
-}
-
-.range {
-  display: block;
-  width: 40%;
-  height: 5px;
-  margin: 2rem auto;
-  -webkit-appearance: none;
-  outline: 0;
-  background: #cccccc;
-  cursor: pointer;
-
-  &::-webkit-slider-thumb {
-    width: 10px;
-    height: 15px;
-    -webkit-appearance: none;
-    background: #aaaaaa;
-    cursor: pointer;
-  }
-}
-
-.pagination {
-  margin-bottom: 3rem;
-}
-
-.loading {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  padding: 0.5rem;
-  transform: translate(-50%, -50%);
-  border: 6px solid #f3f3f3;
-  border-top: 6px solid #333333;
-  border-right: 6px solid #333333;
-  border-radius: 50%;
-  animation: spin 1s linear infinite;
-}
-
-.images {
-  display: grid;
-  grid-template-columns: 33% 33% 33%;
-  grid-gap: 0.5rem;
-
-  figure {
-    overflow: hidden;
-    cursor: pointer;
-
-    img {
-      transition: all 500ms ease;
-
-      &:hover {
-        opacity: 0.8;
-        transform: scale(1.5);
-      }
-    }
-  }
-}
-</style>
---
+++ resources/assets/scripts/components/image.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="column is-one-third">
+    <div v-if=loading class="loading center"></div>
+    <figure class="image image-frame">
+      <img v-show="loading==false" :src=image.url :alt=image.alt @load="loading=false">
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: true
+    };
+  },
+  props: {
+    image: {
+      type    : Object,
+      required: true
+    }
+  }
+};
+</script>
