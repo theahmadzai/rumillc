d485e7edf07d9d7fb15b57a49ef143ec40b1c9bd
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-18 22:37:00 +0500

    responsive

--- resources/assets/scripts/components/product-page.vue
+++ resources/assets/scripts/components/product-page.vue
@@ -1,32 +1,26 @@
 <template>
-  <main class="main-wrapper">
-    <div v-if="!loaded" class="loading"></div>
-    <div v-else class="product-wrapper">
-      <h1 class="category">{{ product.category.name }}</h1>
-      <div class="product">
-          <figure class="image-frame">
-            <img :src="product.image" :alt="product.name">
-          </figure>
-          <div class="details">
-            <h2 class="name">{{ product.name }}</h2>
-            <p class="contents">{{ product.content }}</p>
-            <button class="more">Contact for price</button>
-          </div>
-      </div>
-
-      <writefeedback :id="id"></writefeedback>
-
-      <ratingfeedbacks :id="id"></ratingfeedbacks>
-
-      <listfeedbacks :id="id"></listfeedbacks>
-    </div>
-  </main>
+  <div v-if=!loaded class="loading rel"></div>
+  <div v-else class="product-page">
+    <main class="product">
+        <ImageComponent class="image image-frame" :src=product.image :alt=product.name></ImageComponent>
+        <div class="details">
+          <h1 class="details__name" v-text=product.name></h1>
+          <p class="details__category" v-text=product.category.name></p>
+          <p class="details__info" v-text=product.content></p>
+          <a class="details__more">Contact for price</a>
+        </div>
+    </main>
+    <WriteFeedbackComponent :id=id></WriteFeedbackComponent>
+    <RatingFeedbacksComponent :id=id></RatingFeedbacksComponent>
+    <ListFeedbacksComponent :id=id></ListFeedbacksComponent>
+  </div>
 </template>
 
 <script>
 import WriteFeedback from './writefeedback';
 import ListFeedbacks from './listfeedbacks';
 import RatingFeedbacks from './ratingfeedbacks';
+import Image from './image';
 
 export default {
   props: {
@@ -55,82 +49,10 @@ export default {
     }
   },
   components: {
-    writefeedback  : WriteFeedback,
-    listfeedbacks  : ListFeedbacks,
-    ratingfeedbacks: RatingFeedbacks
+    WriteFeedbackComponent  : WriteFeedback,
+    ListFeedbacksComponent  : ListFeedbacks,
+    RatingFeedbacksComponent: RatingFeedbacks,
+    ImageComponent          : Image
   }
 };
 </script>
-
-<style lang="scss" scoped>
-@import '~@/_settings.scss';
-
-.main-wrapper {
-  position: relative;
-  width: 80%;
-  min-height: 300px;
-  margin: 4rem auto;
-}
-
-.product-wrapper {
-  padding: 5rem;
-  background: #ffffff;
-
-  .category {
-    margin-bottom: 3rem;
-    font-size: 2rem;
-    font-weight: 600;
-    text-align: center;
-    text-transform: uppercase;
-  }
-
-  .product {
-    display: flex;
-    justify-content: space-between;
-
-    .image-frame {
-      flex-basis: 46%;
-      overflow: hidden;
-
-      img {
-        transition: all 300ms ease-in-out;
-
-        &:hover {
-          transform: scale(1.2);
-        }
-      }
-    }
-
-    .details {
-      flex-basis: 50%;
-
-      .name {
-        font-size: 2rem;
-        font-weight: 600;
-      }
-
-      .contents {
-        margin-top: 2rem;
-        margin-bottom: 2rem;
-        font-size: 0.95rem;
-        line-height: 2rem;
-        text-align: justify;
-      }
-
-      .more {
-        padding: 0.5rem 2rem;
-        border: 1px solid #ffffff;
-        background: $secondary-color;
-        font-size: 1rem;
-        font-weight: 600;
-        color: #ffffff;
-        cursor: pointer;
-
-        &:hover {
-          background: $primary-color;
-        }
-      }
-    }
-  }
-}
-</style>
