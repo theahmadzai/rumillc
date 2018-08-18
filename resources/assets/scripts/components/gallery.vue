904135435748e0e643693dd260a1a6bbe2f75eca
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-18 22:36:13 +0500

    cleanup

--- resources/assets/scripts/components/gallery.vue
+++ resources/assets/scripts/components/gallery.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="section">
     <input type="range" class="range-gallery" step="3" min="3" max="9" v-model.number=offset @change=loadImages>
-
     <div v-if=loading class="loading rel"></div>
     <div v-else>
-
-      <pagination :pagination=pagination :offset=offset @paginate=loadImages></pagination>
-
+      <PaginationComponent :pagination=pagination :offset=offset @paginate=loadImages></PaginationComponent>
       <div class="columns is-multiline is-8">
-          <image-component class="column is-one-third image-frame"
+          <ImageComponent class="column is-one-third image-frame"
             v-for="(image, key) in images"
             :key=key
             :src=image.url
             :alt=image.title
             @click.native="index = key">
-          </image-component>
+          </ImageComponent>
       </div>
-
-      <gallery :images=images :options=options :index=index @close="index = null"></gallery>
-
+      <VueGalleryComponent :images=images :options=options :index=index @close="index = null"></VueGalleryComponent>
     </div>
   </div>
 </template>
@@ -65,9 +60,9 @@ export default {
     }
   },
   components: {
-    gallery          : VueGallery,
-    pagination       : Pagination,
-    'image-component': Image
+    VueGalleryComponent: VueGallery,
+    PaginationComponent: Pagination,
+    ImageComponent     : Image
   }
 };
 </script>
