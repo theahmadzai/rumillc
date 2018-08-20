5203406dfe48573a8b19054065d37959b989b4cd
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-20 23:39:58 +0500

    fixed address

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,6 @@
         <div class="column is-one-half contact">
             <h1>Contact Info</h1>
             <p><strong>@lang('contact.address.h'):</strong> @lang('contact.address.m')</p>
-            <p><strong>@lang('contact.factory.h'):</strong> @lang('contact.factory.m')</p>
             <p><strong>@lang('contact.phone.h'):</strong> @lang('contact.phone.m')</p>
             <p><strong>@lang('contact.email.h'):</strong> @lang('contact.email.m')</p>
         </div>
