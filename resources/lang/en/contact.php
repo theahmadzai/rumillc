cf1bf20d8175b1c2a5446dbfef463bb1dcd47f9b
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-20 23:41:56 +0500

    fixed address

--- resources/lang/en/contact.php
+++ resources/lang/en/contact.php
@@ -5,10 +5,6 @@ return [
         'h' => 'Main office',
         'm' => 'Nasajee Compound, Next to Juma Mohammadi Industrial Park, Karte Naw Road, Kabul-Afghanistan',
     ],
-    'factory' => [
-        'h' => 'Factory',
-        'm' => 'Nasajee Compound, Next to Juma Mohammadi Industrial Park, Karte Naw Road, Kabul-Afghanistan'
-    ],
     'phone' => [
         'h' => 'Contact',
         'm' =>  '+93 (0) 784516129'
