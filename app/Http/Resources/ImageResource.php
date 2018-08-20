e1399107b53df1bba40aa2701a26fa761bd92b91
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-08-20 23:41:21 +0500

    added description

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -18,6 +18,7 @@ class ImageResource extends JsonResource
             'id'    => $this->id,
             'title' => $this->title,
             'url'   => $this->url,
+            'description' => $this->description
         ];
     }
 }
