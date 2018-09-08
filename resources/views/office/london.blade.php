3caed1b92d63b694b1a7ea469ec583d972279d12
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-09-09 01:05:14 +0500

    Updated

--- resources/views/office/london.blade.php
+++ resources/views/office/london.blade.php
@@ -3,10 +3,12 @@
 @section('title', 'London')
 
 @section('content')
-    <div class="section content">
-        <div class="construction">
-            <i class="fas fa-wrench fa-3x"></i>
-            <p>Under construction, This page will be available soon.</p>
+    <div class="section">
+        <div class="content" style="width:60%;margin:0 auto;background:#fff;padding:2rem;">
+            <p><strong>Name: </strong>Meyajan Nikben</p>
+            <p><strong>Email: </strong>meyajan@hotmail.com</p>
+            <p><strong>Phone: </strong>+44 787 872 2301</p>
+            <p><strong>Address: </strong>58 Segrave close, Stepney Green, London, E1 0ql, England, United Kingdom</p>
         </div>
     </div>
 @endsection
