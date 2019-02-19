74a7aa5d83c2aca2eef1da78554722771ea843ae
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-02-19 22:42:43 +0500

    eslint config immortal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,12 @@
 module.exports = {
-  extends      : [ 'eslint:recommended', 'immortal', 'plugin:vue/recommended' ],
-  plugins      : [ 'vue' ],
   parserOptions: {
-    ecmaVersion                : 9,
-    sourceType                 : 'module',
-    allowImportExportEverywhere: true,
-    ecmaFeatures               : {
-      jsx: false
-    }
+    sourceType: 'module'
   },
   env: {
     browser: true,
-    es6    : true
+    es6: true
   },
-  rules: {
-    'key-spacing': [
-      'error',
-      {
-        align: {
-          on: 'colon'
-        },
-        multiLine: {
-          beforeColon: false,
-          afterColon : true
-        }
-      }
-    ]
-  }
+  plugins: [ 'vue' ],
+  extends: [ 'eslint:recommended', 'immortal', 'plugin:vue/recommended' ],
+  rules: {}
 };
