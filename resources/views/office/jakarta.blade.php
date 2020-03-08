ee1a8872fb126db0c3a271969b454f514f3bb8e3
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-03-08 16:24:50 +0500

    updated

--- resources/views/office/jakarta.blade.php
+++ resources/views/office/jakarta.blade.php
@@ -5,9 +5,12 @@
 @section('content')
     <div class="section">
         <div class="content" style="width:60%;margin:0 auto;background:#fff;padding:2rem;">
-            <p><strong>Name: </strong>Alfian Fariz</p>
-            <p><strong>Phone: </strong>+6 285 819 830 027</p>
-            <p><strong>Address: </strong>Jalan anggrek cendrawasih 8A, Slipi kebon sayur, RT/RW 016/003 No.10 Kelurahan Kemanggisan Kecamatan Palmerah, Jakarta barat, DKI Jakarta</p>
+            <p><strong>Company Name: </strong>PT. Cad Camp Nusantara</p>
+            <p><strong>Company Representative: </strong>Jawa Barat</p>
+            <p><strong>Company Distributor: </strong>Ahmad Waseem</p>
+            <p><strong>Phone: </strong>(021) 885 2769</p>
+            <p><strong>Email Address: </strong>dawoodsaffron.indo@gmail.com</p>
+            <p><strong>Address: </strong>Perum Pondok Cipta Blok D No 54, Bintara, Bekasi Barat</p>
         </div>
     </div>
 @endsection
