bbc1e37c98e462f842800dc3e5d3f75cde73649a
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-07-06 22:02:55 +0500

    Use useRating hook

--- resources/js/components/ProductPreview.js
+++ resources/js/components/ProductPreview.js
@@ -2,13 +2,11 @@ import React, { useState } from 'react'
 import { Card, Statistic, Row, Col, Rate } from 'antd'
 import { LikeOutlined } from '@ant-design/icons'
 import { Link } from '@reach/router'
+import useRating from './useRating'
 
 const ProductPreview = ({ slug, name, thumbnail, feedbacks }) => {
   const [loading, setLoading] = useState(true)
-
-  const rating = feedbacks
-    .map(f => Number.parseFloat(f.rating))
-    .reduce((t, f) => t + f, 0)
+  const [rating] = useRating(feedbacks.map(f => Number.parseFloat(f.rating)))
 
   return (
     <Link to={'/products/' + slug}>
@@ -24,13 +22,13 @@ const ProductPreview = ({ slug, name, thumbnail, feedbacks }) => {
         <Card.Meta title={name} />
         <Row gutter={16}>
           <Col span={12}>
-            <Statistic title="Feedback" value={rating} prefix={<LikeOutlined />} />
+            <Statistic title="Feedback" value={rating.totalRating} prefix={<LikeOutlined />} />
           </Col>
           <Col span={12}>
             <Statistic title="Available" value={100} suffix="/100" />
           </Col>
         </Row>
-        <Rate disabled allowHalf defaultValue={5} value={rating} />
+        <Rate disabled allowHalf defaultValue={5} value={rating.overallRating} />
       </Card>
     </Link>
   )
