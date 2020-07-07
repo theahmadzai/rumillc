0a5dfcd8f3e48c335ba08a190f3da05dc4d2d93a
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-07-07 19:22:20 +0500

    StoreFeedbackRequest

---
+++ app/Http/Requests/StoreFeedbackRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreFeedbackRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'    => 'required',
+            'title'   => 'required',
+            'message' => 'required',
+            'rating'  => 'required|numeric|between:1,5',
+            'product_id' => 'required|exists:products,id',
+        ];
+    }
+}
