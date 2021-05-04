2f6a14b3d083e4d5152392e7b7c1b0e5bbd2529a
Muhammad Javed <theahmadzai@hotmail.com>
2021-05-05 02:29:14 +0500

    Add SEO component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { BackTop } from 'antd'
-import useSiteMetadata from '../hooks/use-sitemetadata'
+import SEO from './seo'
 import Header from './header'
 import Footer from './footer'
 import * as styles from './layout.module.css'
 
 export default function Layout({ children, ...props }) {
-  const { title, description } = useSiteMetadata()
-
   return (
     <div className={styles.layout}>
       <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
         <link
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;600&display=swap"
           rel="stylesheet"
         />
       </Helmet>
+      <SEO />
       <Header />
       <main {...props}>{children}</main>
       <Footer />
--- src/hooks/use-sitemetadata.js
+++ src/hooks/use-sitemetadata.js
@@ -7,6 +7,7 @@ export default function useSiteMetadata() {
         siteMetadata {
           name
           title
+          siteUrl
           description
           address
           contacts {
